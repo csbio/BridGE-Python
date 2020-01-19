b52f4aa70f4497ff6c4a14ae745ba6c8257793cf
Mehrad <hajia018@umn.edu>
2020-01-19 15:54:43 -0600

    interaction network calss added

---
+++ classes/InteractionNetwork.py
@@ -0,0 +1,4 @@
+class InteractionNetwork():
+        def __init__(self,risk_network,protective_network):
+                self.risk = risk_network
+                self.protective = protective_network
