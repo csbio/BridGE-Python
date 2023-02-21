0fa47dd8fc7e1c4dbb10b39dd1b98ce60f1b587a
Mehrad Hajiaghabozorgi <chahkeshkosh@Mehrads-MacBook-Air.local>
2023-02-20 21:46:07 -0600

    cassi.sh setup

--- setup.sh
+++ setup.sh
@@ -9,9 +9,11 @@ mv cyadd*.so corefuns/cyadd.so
 #chmod u+x preprocessgwas.sh
 chmod u+x scripts/*.sh
 chmod u+x scripts/plink
+chmod u+x cassi.sh
 
 export CURRENTDIR=`pwd`
 export PYTHONPATH=$CURRENTDIR/scripts
 export PATH=$CURRENTDIR/scripts/:$PATH
+export PATH=$CURRENTDIR/:$PATH
 alias plink=$CURRENTDIR/scripts/plink
 
